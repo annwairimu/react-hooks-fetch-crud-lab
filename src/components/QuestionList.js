ee6296669b7b0cc8b75badc2f27254b6f8c5be32
Ann Wangui Wairimu <ann99wangui@gmail.com>
2023-04-27 20:32:00 +0300

    done

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,35 @@
-import React from "react";
+
+import React, { useState, useEffect } from 'react';
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    async function fetchQuestions() {
+      const response = await fetch('http://localhost:4000/questions');
+      const data = await response.json();
+      setQuestions(data);
+    }
+    fetchQuestions();
+  }, []);
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <li key={question.id}>
+            <h2>{question.prompt}</h2>
+            <ul>
+              {question.answers.map((answer, index) => (
+                <li key={index}>{answer}</li>
+              ))}
+            </ul>
+            <p>Correct answer: {question.answers[question.correctIndex]}</p>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
-
 export default QuestionList;
